462ad76029dad928163a82a2bc826b2368e092df
callgp <callgp@gmail.com>
2020-04-19 23:32:28 -0400

    04192020 at 1132pm

--- src/main/java/com/gp/patterns/mvc/MvcApplication.java
+++ src/main/java/com/gp/patterns/mvc/MvcApplication.java
@@ -8,6 +8,7 @@ public class MvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcApplication.class, args);
+		System.out.println("04192020 at5 1130pm");
 	}
 
 }
